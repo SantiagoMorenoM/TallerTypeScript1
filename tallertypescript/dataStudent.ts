75118fa90c7c0e6a34e6d008a958684c7910f5b3
s.morenom <53950575+SantiagoMorenoM@users.noreply.github.com>
2020-03-31 16:05:23 -0500

    Add files via upload

---
+++ tallertypescript/dataStudent.ts
@@ -0,0 +1,6 @@
+import { Student } from './student.js';
+
+export const dataStudents = [
+  new Student(201814353,1010089483, 19,"13#101-88","3157443164")
+ 
+]
---
+++ tallertypescript/scripts/dataCourses.js
@@ -0,0 +1,9 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Desarrollo de Software en Equipo", "Cesar Augusto Forero Gonzales", 3),
+    new Course("Sistemas transaccionales", "Claudia Lucia Jimenez Guarin", 3),
+    new Course("Diseño y Analisis de Algoritmos", "Rodrigo Cardoso Rodriguez", 3),
+    new Course("Japones 3", "Paula Eslava Lozzi", 3),
+    new Course("Calculo Vectorial", "Mauricio Velasco Gregory", 3),
+    new Course("Biologia Celular", "Zayra Viviana Garavito Aguilar", 3),
+];
